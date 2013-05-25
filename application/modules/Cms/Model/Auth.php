80de1306f1f8189d8c9250edc73cdbd763ca3099
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2013-05-26 01:01:45 +0200

    poprawka autoryzacji ID

--- application/modules/Cms/Model/Auth.php
+++ application/modules/Cms/Model/Auth.php
@@ -49,18 +49,25 @@ class Cms_Model_Auth implements Mmi_Auth_Model_Interface {
 	}
 
 	public static function idAuthenticate($id) {
-		$this->get(array(
+		$record = Cms_Model_Auth_Dao::findFirst(array(
 			array('id', $id, '=', 'OR'),
-			array('username', ((intval($id) > 0) ? intval($id) : -1), '=', 'OR'),
+			array('username', $id, '=', 'OR'),
 			array('email', $id, '=', 'OR')
 		));
-		if ($this->getId() > 0) {
-			$this->roles = $this->getRoles($this->getId());
-			$this->_notifyLogin($this);
-			return $this;
+		if ($record === null) {
+			return false;
 		}
-		$this->_notifyBadLogin($id);
-		return false;
+		$record->roles = Cms_Model_Auth_Role_Dao::findPairsRolesByAuthId($record->id);
+		$record->lastIp = (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null);
+		$record->lastLog = date('Y-m-d H:i:s');
+		Cms_Model_Log_Dao::add('login', array(
+			'object' => 'cms_auth',
+			'objectId' => $record->id,
+			'cms_auth_id' => $record->id,
+			'success' => true,
+			'message' => 'LOGGED (ID): ' . $record->username
+		));
+		return $record;
 	}
 
 	public static function deauthenticate() {
