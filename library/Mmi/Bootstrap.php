8393a7aec3a38e615e38d312963c631f06b04a56
Mariusz Mi?ejko <mmilejko@empik.com>
2013-06-24 15:28:58 +0200

    poprawka w bootstrapie (wydzielenie funkcji)

--- library/Mmi/Bootstrap.php
+++ library/Mmi/Bootstrap.php
@@ -82,13 +82,10 @@ class Mmi_Bootstrap {
 
 		//obsługa włączonych magic quotes
 		if (ini_get('magic_quotes_gpc')) {
-			function stripslashes_gpc(&$value) {
-				$value = stripslashes($value);
-			}
-			array_walk_recursive($_GET, 'stripslashes_gpc');
-			array_walk_recursive($_POST, 'stripslashes_gpc');
-			array_walk_recursive($_COOKIE, 'stripslashes_gpc');
-			array_walk_recursive($_REQUEST, 'stripslashes_gpc');
+			array_walk_recursive($_GET, array($this, '_stripslashesGpc'));
+			array_walk_recursive($_POST, array($this, '_stripslashesGpc'));
+			array_walk_recursive($_COOKIE, array($this, '_stripslashesGpc'));
+			array_walk_recursive($_REQUEST, array($this, '_stripslashesGpc'));
 		}
 
 		//ustawienie lokalizacji
@@ -236,6 +233,14 @@ class Mmi_Bootstrap {
 		$front = $this->registerPlugins();
 		$front->dispatch();
 		ob_end_flush();
+	}
+
+	/**
+	 * 
+	 * @param type $value
+	 */
+	private function _stripslashesGpc(&$value) {
+		$value = stripslashes($value);
 	}
 
 }
