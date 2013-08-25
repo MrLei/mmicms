1bbba689e412cfafd4daa5448937f93c09aeb4b8
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2013-08-25 17:37:03 +0200

    Ujednolicenie filtra escape

--- library/Mmi/Filter/Escape.php
+++ library/Mmi/Filter/Escape.php
@@ -8,10 +8,9 @@
  * Licencja jest dostępna pod adresem: http://www.hqsoft.pl/new-bsd
  * W przypadku problemów, prosimy o kontakt na adres office@hqsoft.pl
  *
- * Mmi/View/Helper/Escape.php
+ * Mmi/Filter/Escape.php
  * @category   Mmi
- * @package    Mmi_View
- * @subpackage Helper
+ * @package    Mmi_Filter
  * @copyright  Copyright (c) 2010 HQSoft Mariusz Miłejko (http://www.hqsoft.pl)
  * @author     Mariusz Miłejko <mariusz@milejko.pl>
  * @version    $Id$
@@ -19,23 +18,31 @@
  */
 
 /**
- * Helper normalizowania ciągów znaków
+ * Klasa filtracji pól tekstowych z HTML i znaków zabronionych np. w polu alt=""
  * @category   Mmi
- * @package    Mmi_View
- * @subpackage Helper
+ * @package    Mmi_Filter
  * @license    http://www.hqsoft.pl/new-bsd     New BSD License
  */
-class Mmi_View_Helper_Escape extends Mmi_View_Helper_Abstract {
+class Mmi_Filter_Escape extends Mmi_Filter_Abstract {
 
 	/**
-	 * Usuwa tagi z ciągu znaków
-	 * @see Mmi_Filter_Escape
-	 * @param string $input ciąg wejściowy
-	 * @return string
+	 * Filtracja pola ze znaków specjalnych
+	 * @param mixed $value wartość
+	 * @throws Exception jeśli filtrowanie $value nie jest możliwe
+	 * @return mixed
 	 */
-	public function escape($input) {
-		$escape = new Mmi_Filter_Escape();
-		return $escape->filter($input);
+	public function filter($value) {
+
+		return str_replace(array(
+			'"',
+			'<',
+			'>',
+		), array(
+			'&#34;',
+			'&#60;',
+			'&#62;',
+		), strip_tags($value));
+
 	}
-	
+
 }
