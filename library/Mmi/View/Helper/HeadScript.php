8634500919961e9438c3760f8d3d61411a9f424c
Krzysztof Policha <kpolicha@empik.com>
2013-08-26 15:02:01 +0200

    HeadScript: escape znak?w specjalnych.

--- library/Mmi/View/Helper/HeadScript.php
+++ library/Mmi/View/Helper/HeadScript.php
@@ -83,7 +83,7 @@ class Mmi_View_Helper_HeadScript extends Mmi_View_Helper_AbstractHead {
 						$value .= '?crc=' . $crc;
 					}
 				}
-				$html .= $key. '="' . $value . '" ';
+				$html .= htmlentities($key). '="' . htmlentities($value) . '" ';
 			}
 			$html .= '>';
 			if (isset($scriptContent)) {
