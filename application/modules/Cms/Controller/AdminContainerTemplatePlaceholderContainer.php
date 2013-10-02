1e9fa82c7752ca27a4674f53cb89e2bb2e68aa44
Mariusz Mi?ejko <mmilejko@empik.com>
2013-10-02 16:57:34 +0200

    kontenery cms

---
+++ application/modules/Cms/Controller/AdminContainerTemplatePlaceholderContainer.php
@@ -0,0 +1,29 @@
+<?php
+
+class Cms_Controller_AdminContainerTemplatePlaceholderContainer extends MmiCms_Controller_Admin {
+
+	public function indexAction() {
+		$this->view->grid = new Cms_Plugin_ContainerTemplatePlaceholderContainerGrid();
+		//placeholdery tylko dla danego szablonu
+		if ($this->_getParam('containerId')) {
+			$this->view->grid->setOption('bind', array(array('cms_container_id', $this->_getParam('containerId'), '=')));
+		}
+	}
+
+	public function editAction() {
+		$form = new Cms_Form_Admin_Container_Template_Placeholder_Container($this->_getParam('id'), array('containerId' => $this->_getParam('containerId')));
+		if ($form->isSaved()) {
+			$this->_helper->messenger('Placeholder zapisany poprawnie', true);
+			$this->_helper->redirector('edit', 'adminContainer', 'cms', array('id' => $form->getRecord()->cms_container_id), true);
+		}
+	}
+
+	public function deleteAction() {
+		$record = Cms_Model_Container_Template_Placeholder_Container_Dao::findPk($this->_getParam('id'));
+		if ($record && $record->delete()) {
+			$this->_helper->messenger('Poprawnie usunięto placeholder', true);
+		}
+		$this->_helper->redirector('edit', 'adminContainer', 'cms', array('id' => $record->cms_container_id), true);
+	}
+
+}
---
+++ application/skins/default/cms/scripts/adminContainerTemplate/index.tpl
@@ -0,0 +1,9 @@
+<div class="content-box">
+	<div class="content-box-header">
+		<h3>{#Szablony kontenerów#}</h3>
+		<div class="clear"></div>
+	</div>
+	<div class="content-box-content">
+		{$grid}
+	</div>
+</div>
