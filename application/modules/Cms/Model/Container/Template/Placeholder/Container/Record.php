ae443aceb2f57abdcd7f8747f92633e15d26c6d6
Mariusz Mi?ejko <mmilejko@empik.com>
2013-10-03 15:19:19 +0200

    Scal z default

--- application/modules/Cms/Model/Container/Template/Placeholder/Container/Record.php
+++ application/modules/Cms/Model/Container/Template/Placeholder/Container/Record.php
@@ -1,5 +1,14 @@
 <?php
 
 class Cms_Model_Container_Template_Placeholder_Container_Record extends Mmi_Dao_Record {
-	
+
+	public function save() {
+		$object = explode('_', $this->object);
+		unset($this->object);
+		$this->module = isset($object[0]) ? $object[0] : 'default';
+		$this->controller = isset($object[1]) ? $object[1] : 'index';
+		$this->action = isset($object[2]) ? $object[2] : 'index';
+		return parent::save();
+	}
+
 }
