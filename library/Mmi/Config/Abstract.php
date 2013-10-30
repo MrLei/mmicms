a085d884c3de8c351c8eaace0dec614f39bb3338
Mariusz Mi?ejko <mmilejko@empik.com>
2013-10-30 14:06:36 +0100

    Dodanie konfiguracji Aplikacji, Cache, Db i Session

---
+++ library/Mmi/Config/Abstract.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Mmi
+ *
+ * LICENSE
+ *
+ * Ten plik źródłowy objęty jest licencją BSD bez klauzuli ogłoszeniowej.
+ * Licencja jest dostępna pod adresem: http://www.hqsoft.pl/new-bsd
+ * W przypadku problemów, prosimy o kontakt na adres office@hqsoft.pl
+ *
+ * Mmi/Config/Abstract.php
+ * @category   Mmi
+ * @package    Mmi_Config
+ * @copyright  Copyright (c) 2010 HQSoft Mariusz Miłejko (http://www.hqsoft.pl)
+ * @author     Mariusz Miłejko <mariusz@milejko.pl>
+ * @version    $Id$
+ * @license    http://www.hqsoft.pl/new-bsd     New BSD License
+ */
+
+/**
+ * Abstrakcyjna klasa konfiguracji Mmi
+ * @category   Mmi
+ * @package    Mmi_Config
+ * @license    http://www.hqsoft.pl/new-bsd     New BSD License
+ */
+
+abstract class Mmi_Config_Abstract {
+
+}
