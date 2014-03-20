8738082916a1d634bb4fad212a33449ca0b0b10a
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2014-03-20 22:11:43 +0100

    wersje j?zykowe

--- library/MmiCms/Controller/Admin.php
+++ library/MmiCms/Controller/Admin.php
@@ -12,12 +12,16 @@ class MmiCms_Controller_Admin Extends Mmi_Controller_Action {
 	public function __construct(Mmi_Controller_Request $request) {
 		
 		//acl dla admina
-		$this->view = Mmi_Controller_Front::getInstance()->getView();
 		if (!Default_Registry::$acl->isAllowed(Default_Registry::$auth->getRoles(), 'admin:index:index')) {
-			header('Location: ' . $this->view->baseUrl . '/admin');
-			exit;
+			$this->_helper->redirector('index', 'index', 'admin', array(), true);
 		}
+		
+		$session = new Mmi_Session_Namespace('cms-language');
+		$lang = in_array($session->lang, Default_Registry::$config->application->languages) ? $session->lang : null;
+		Mmi_Controller_Front::getInstance()->getRequest()->lang = $lang;
+		$request->lang = $lang;
 
+		$this->view = Mmi_Controller_Front::getInstance()->getView();
 		$this->view->baseSkin = Default_Registry::$config->application->skin;
 		$this->view->baseModule = 'admin';
 
