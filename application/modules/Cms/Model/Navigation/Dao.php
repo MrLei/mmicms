d2542c52cf59305129e48193ece98a45e56837ab
Mariusz Mi?ejko <mmilejko@empik.com>
2014-03-24 20:06:45 +0100

    porz?dki po refaktoryzacji

--- application/modules/Cms/Model/Navigation/Dao.php
+++ application/modules/Cms/Model/Navigation/Dao.php
@@ -28,19 +28,17 @@ class Cms_Model_Navigation_Dao extends Mmi_Dao {
 
 	public static function getMultiOptions() {
 		$multiOptions = array();
-		/*foreach (self::$_nested->flat(self::$_nested->getStructure()) as $leaf) {
-			if ($leaf['label'] == '')
-				$leaf['label'] = '---';
-			$space = '';
-			for ($i = 0; $i < $leaf['level']; $i++) {
-				$space .= '&nbsp;&nbsp;';
-			}
-			$space .= '| ';
-			$multiOptions[$leaf['id']] = $space . $leaf['label'];
-		}*/
-		return $multiOptions;
+		$q = self::newQuery()
+			->orderAsc('parent_id')
+			->orderAsc('order');
+		self::_langQuery($q);
+		return self::findPairs('id', 'label', $q);
 	}
 
+	/**
+	 * Dodaje do konfiguracji dane z bazy danych
+	 * @param Mmi_Navigation_Config $config
+	 */
 	public static function decorateConfiguration(Mmi_Navigation_Config $config) {
 		$q = self::newQuery()
 			->orderAsc('parent_id')
@@ -59,55 +57,6 @@ class Cms_Model_Navigation_Dao extends Mmi_Dao {
 		}
 	}
 
-	protected static function _buildChildren(Cms_Model_Navigation_Record $record, Mmi_Navigation_Config_Element $element, array $objectArray) {
-		foreach ($objectArray as $key => $child) {/* @var $child Cms_Model_Navigation_Record */
-			if ($child->parent_id != $record->id) {
-				continue;
-			}
-			$childElement = Mmi_Navigation_Config::newElement($child->id);
-			self::_setNavigationElementFromRecord($child, $childElement);
-			$element->addChild($childElement);
-			unset($objectArray[$key]);
-			self::_buildChildren($child, $childElement, $objectArray);
-		}
-	}
-
-	protected static function _setNavigationElementFromRecord(Cms_Model_Navigation_Record $record, Mmi_Navigation_Config_Element $element) {
-
-		$https = null;
-		if ($record->https === 0) {
-			$https = false;
-		} elseif ($record->https === 1) {
-			$https = true;
-		}
-
-		$params = array();
-		parse_str($record->params, $params);
-
-		$element
-			->setAbsolute($record->absolute ? true : false)
-			->setAction($record->action ?: null)
-			->setBlank($record->blank ? true : false)
-			->setController($record->controller ?: null)
-			->setDateEnd($record->dateEnd ?: null)
-			->setDateStart($record->dateStart ?: null)
-			->setDescription($record->description ?: null)
-			->setDisabled($record->active ? false : true)
-			->setHttps($https)
-			->setIndependent($record->independent ?: null)
-			->setKeywords($record->keywords ?: null)
-			->setLabel($record->label ?: null)
-			->setLang($record->lang ?: null)
-			->setModule($record->module ?: null)
-			->setNofollow($record->nofollow ?: null)
-			->setParams($params)
-			->setTitle($record->title ?: null)
-			->setUri($record->uri ?: null)
-			->setVisible($record->visible ? true : false)
-		;
-		return $element;
-	}
-
 	/**
 	 * Sortuje po zserializowanej tabeli identyfikatorów
 	 * @param array $serial tabela identyfikatorów
@@ -122,6 +71,54 @@ class Cms_Model_Navigation_Dao extends Mmi_Dao {
 			$record->save();
 		}
 		return true;
+	}
+
+	protected static function _buildChildren(Cms_Model_Navigation_Record $record, Mmi_Navigation_Config_Element $element, array $objectArray) {
+		foreach ($objectArray as $key => $child) {/* @var $child Cms_Model_Navigation_Record */
+			if ($child->parent_id != $record->id) {
+				continue;
+			}
+			$childElement = Mmi_Navigation_Config::newElement($child->id);
+			self::_setNavigationElementFromRecord($child, $childElement);
+			$element->addChild($childElement);
+			unset($objectArray[$key]);
+			self::_buildChildren($child, $childElement, $objectArray);
+		}
+	}
+
+	protected static function _setNavigationElementFromRecord(Cms_Model_Navigation_Record $record, Mmi_Navigation_Config_Element $element) {
+		$https = null;
+		if ($record->https === 0) {
+			$https = false;
+		} elseif ($record->https === 1) {
+			$https = true;
+		}
+
+		$params = array();
+		parse_str($record->params, $params);
+
+		$element
+			->setAbsolute($record->absolute ? true : false)
+			->setAction($record->action ? : null)
+			->setBlank($record->blank ? true : false)
+			->setController($record->controller ? : null)
+			->setDateEnd($record->dateEnd ? : null)
+			->setDateStart($record->dateStart ? : null)
+			->setDescription($record->description ? : null)
+			->setDisabled($record->active ? false : true)
+			->setHttps($https)
+			->setIndependent($record->independent ? : null)
+			->setKeywords($record->keywords ? : null)
+			->setLabel($record->label ? : null)
+			->setLang($record->lang ? : null)
+			->setModule($record->module ? : null)
+			->setNofollow($record->nofollow ? : null)
+			->setParams($params)
+			->setTitle($record->title ? : null)
+			->setUri($record->uri ? : null)
+			->setVisible($record->visible ? true : false)
+		;
+		return $element;
 	}
 
 	protected static function _langQuery(Mmi_Dao_Query $q) {
--- application/skins/default/cms/scripts/adminNavigation/index.tpl
+++ application/skins/default/cms/scripts/adminNavigation/index.tpl
@@ -8,38 +8,37 @@
 	</div>
 	<div class="content-box-content clearfix">
 		{if $navigation}
-		<div class="list-path">
-			{foreach $navigation.parents as $parent}
-			<a href="{url(array('id' => $parent['id']))}">{if !$parent.label}{#Katalog główny#}{else}{$parent.label}{/if}</a> &raquo;
-			{/foreach}
-			{if !php_isset($navigation.label)}
-			{#Katalog główny#}
+			{if $navigation.parents}
+					{foreach $navigation['parents'] as $child}
+						<a href="{url(array('id' => $child['id']))}">{if $child.label}{$child.label}{else}{#Katalog główny#}{/if}</a> &raquo;
+					{/foreach}
+					{if system_isset($navigation.label)}
+						{$navigation.label}
+					{/if}
 			{else}
-			{$navigation.label}
+				{#Katalog główny#}
 			{/if}
-			[<span>{#poziom#}: {$navigation.level}</span>]
-		</div>
-		{if !php_empty($navigation.children)}
-		<ul class="list" id="navigation-list">
-			{foreach $navigation.children as $id => $child}
-			<li id="navigation-item-{$id}" class="navigation-{$child.type}">
-				<div><i class="icon-{if $child.type == 'folder'}folder-close{elseif $child.type == 'simple'}book{elseif $child.type == 'container'}magic{elseif $child.type == 'link'}globe{else}cogs{/if}"></i> {if $child['lang']}<img class="language" src="{$baseUrl}/default/geo/image/country/{$child['lang']}-ico-16.gif" /> {/if}<a href="{url(array('id' => $child['id']))}">{$child.label}</a></div>
-                <div>{if $child.disabled}<i class="icon-minus-sign"></i> {#wyłączony#}{else}{if $child.visible==1}<i class=" icon-eye-open"></i> {#widoczny#}{else}<i class=" icon-eye-close"></i> {#ukryty#}{/if}{/if}</div>
-				<a href="{@module=cms&controller=adminNavigation&action=edit&action=edit&type={$child.type}&id={$child.id}@}" class="button edit"><i class="icon-edit"></i> {#edytuj#}</a>
-				<a href="{@module=cms&controller=adminNavigation&action=edit&action=delete&id={$child.id}@}" class="button delete confirm" title="{#Czy na pewno usunąć pozycję menu wraz z podmenu#}?"><i class="icon-remove-sign"></i> {#usuń#}</a>
-			</li>
-			{/foreach}
-		</ul>
+			{if !php_empty($navigation.children)}
+				<ul class="list" id="navigation-list">
+					{foreach $navigation.children as $id => $child}
+					<li id="navigation-item-{$id}" class="navigation-{$child.type}">
+						<div><i class="icon-{if $child.type == 'folder'}folder-close{elseif $child.type == 'simple'}book{elseif $child.type == 'container'}magic{elseif $child.type == 'link'}globe{else}cogs{/if}"></i> {if $child['lang']}<img class="language" src="{$baseUrl}/default/geo/image/country/{$child['lang']}-ico-16.gif" /> {/if}<a href="{url(array('id' => $child['id']))}">{$child.label}</a></div>
+						<div>{if $child.disabled}<i class="icon-minus-sign"></i> {#wyłączony#}{else}{if $child.visible==1}<i class=" icon-eye-open"></i> {#widoczny#}{else}<i class=" icon-eye-close"></i> {#ukryty#}{/if}{/if}</div>
+						<a href="{@module=cms&controller=adminNavigation&action=edit&action=edit&type={$child.type}&id={$child.id}@}" class="button edit"><i class="icon-edit"></i> {#edytuj#}</a>
+						<a href="{@module=cms&controller=adminNavigation&action=edit&action=delete&id={$child.id}@}" class="button delete confirm" title="{#Czy na pewno usunąć pozycję menu wraz z podmenu#}?"><i class="icon-remove-sign"></i> {#usuń#}</a>
+					</li>
+				{/foreach}
+			</ul>
 		{else}
-		<p class="navigation-empty">{#Brak kategorii, możesz dodać nową klikając poniższy przycisk#}:</p>
+			<p>{#Brak dodanych elementów#}.</p>
 		{/if}
 		<a class="button add" href="{@module=cms&controller=adminNavigation&action=edit&type=cms&parent={$navigation.id}@}"><i class="icon-cogs"></i> {#dodaj obiekt cms#}</a>
 		<a class="button add" href="{@module=cms&controller=adminNavigation&action=edit&type=simple&parent={$navigation.id}@}"><i class="icon-book"></i> {#dodaj artykuł#}</a>
 		<a class="button add" href="{@module=cms&controller=adminNavigation&action=edit&type=container&parent={$navigation.id}@}"><i class="icon-magic"></i> {#dodaj stronę CMS#}</a>
 		<a class="button add" href="{@module=cms&controller=adminNavigation&action=edit&type=link&parent={$navigation.id}@}"><i class="icon-globe"></i> {#dodaj link#}</a>
 		<a class="button add" href="{@module=cms&controller=adminNavigation&action=edit&type=folder&parent={$navigation.id}@}"><i class="icon-folder-close"></i> {#dodaj folder#}</a>
-		{else}
+	{else}
 		{#Brak danych#}.
-		{/if}
-	</div>
+	{/if}
+</div>
 </div>
