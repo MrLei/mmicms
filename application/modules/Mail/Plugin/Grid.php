16f6a684530730f0c57f30efd5429b9bbd53d3bc
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2014-04-10 13:16:17 +0200

    Scal z stg

--- application/modules/Mail/Plugin/Grid.php
+++ application/modules/Mail/Plugin/Grid.php
@@ -2,43 +2,43 @@
 
 class Mail_Plugin_Grid extends Mmi_Grid {
 
-    protected $_daoName = 'Mail_Model_Dao';
+	protected $_daoName = 'Mail_Model_Dao';
 
-    public function init() {
+	public function init() {
 
 		$q = new Mmi_Dao_Query();
 		$this->setInitialQuery($q->orderDesc('id'));
 
-        $this->addColumn('checkbox', 'active', array(
-                'label' => 'Wysłany',
-        ));
+		$this->addColumn('checkbox', 'active', array(
+			'label' => 'Wysłany',
+		));
 
 		$this->addColumn('text', 'dateAdd', array(
-                'label' => 'Data dodania',
-        ));
+			'label' => 'Data dodania',
+		));
 
-        $this->addColumn('text', 'dateSent', array(
-                'label' => 'Data wysłania',
-        ));
+		$this->addColumn('text', 'dateSent', array(
+			'label' => 'Data wysłania',
+		));
 
 		$this->addColumn('text', 'to', array(
-                'label' => 'Do',
-        ));
+			'label' => 'Do',
+		));
 
-        $this->addColumn('text', 'subject', array(
-                'label' => 'Temat',
-        ));
+		$this->addColumn('text', 'subject', array(
+			'label' => 'Temat',
+		));
 
-        $this->addColumn('text', 'fromName', array(
-                'label' => 'Od',
-        ));
+		$this->addColumn('text', 'fromName', array(
+			'label' => 'Od',
+		));
 
-        $this->addColumn('buttons', 'buttons', array(
-                'label' => 'operacje',
-                'links' => array(
-                        'edit' => null
-                )
-        ));
-    }
+		$this->addColumn('buttons', 'buttons', array(
+			'label' => 'operacje',
+			'links' => array(
+				'edit' => null
+			)
+		));
+	}
 
 }
--- application/modules/Mail/Plugin/ServerGrid.php
+++ application/modules/Mail/Plugin/ServerGrid.php
@@ -2,41 +2,41 @@
 
 class Mail_Plugin_ServerGrid extends Mmi_Grid {
 
-    protected $_daoName = 'Mail_Model_Server_Dao';
+	protected $_daoName = 'Mail_Model_Server_Dao';
 
-    public function init() {
+	public function init() {
 
-        $this->addColumn('text', 'address', array(
-            'label' => 'Adres serwera',
-        ));
+		$this->addColumn('text', 'address', array(
+			'label' => 'Adres serwera',
+		));
 
-        $this->addColumn('text', 'port', array(
-            'label' => 'Port',
-        ));
+		$this->addColumn('text', 'port', array(
+			'label' => 'Port',
+		));
 
 		$this->addColumn('text', 'ssl', array(
-            'label' => 'Szyfrowanie',
-        ));
+			'label' => 'Szyfrowanie',
+		));
 
-        $this->addColumn('text', 'username', array(
-            'label' => 'Użytkownik',
-        ));
+		$this->addColumn('text', 'username', array(
+			'label' => 'Użytkownik',
+		));
 
-        $this->addColumn('text', 'from', array(
-            'label' => 'Domyślny nadawca',
-        ));
+		$this->addColumn('text', 'from', array(
+			'label' => 'Domyślny nadawca',
+		));
 
-        $this->addColumn('text', 'dateAdd', array(
-            'label' => 'Data dodania',
-        ));
+		$this->addColumn('text', 'dateAdd', array(
+			'label' => 'Data dodania',
+		));
 
-        $this->addColumn('text', 'dateModify', array(
-            'label' => 'Data modyfikacji',
-        ));
+		$this->addColumn('text', 'dateModify', array(
+			'label' => 'Data modyfikacji',
+		));
 
-        $this->addColumn('buttons', 'buttons', array(
-            'label' => 'operacje',
-        ));
-    }
+		$this->addColumn('buttons', 'buttons', array(
+			'label' => 'operacje',
+		));
+	}
 
 }
