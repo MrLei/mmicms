74bb91be993f1d3c75cbac30c7b76fe6b6e00135
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2014-04-11 14:52:54 +0200

    drobny fix commandline (usuni?cie nadmiaru kodu)

---
+++ application/modules/News/Plugin/Grid.php
@@ -0,0 +1,39 @@
+<?php
+
+class News_Plugin_Grid extends Mmi_Grid {
+
+	protected $_daoName = 'News_Model_Dao';
+	protected $_daoGetMethod = 'findLang';
+	protected $_daoCountMethod = 'countLang';
+
+	public function init() {
+
+		$this->setOption('locked', true);
+		
+		$this->addColumn('text', 'lang', array(
+			'label' => 'język'
+		));
+
+		$this->addColumn('text', 'dateAdd', array(
+			'label' => 'data dodania'
+		));
+
+		$this->addColumn('text', 'dateModify', array(
+			'label' => 'data modyfikacji'
+		));
+
+		$this->addColumn('text', 'title', array(
+			'label' => 'tytuł'
+		));
+
+		$this->addColumn('text', 'text', array(
+			'sortable' => false,
+			'label' => 'treść aktualności'
+		));
+
+		$this->addColumn('buttons', 'buttons', array(
+			'label' => 'operacje'
+		));
+
+	}
+}
---
+++ library/MmiCms/Controller/Admin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * MmiCMS
+ */
+
+/**
+ * Kontroler administracyjny, rozszerza kontroler akcji
+ */
+class MmiCms_Controller_Admin Extends Mmi_Controller_Action {
+
+	public function __construct(Mmi_Controller_Request $request) {
+
+		//acl dla admina
+		if (!Default_Registry::$acl->isAllowed(Default_Registry::$auth->getRoles(), 'admin:index:index')) {
+			$this->_helper->redirector('index', 'index', 'admin', array(), true);
+		}
+
+		//ustawienie języka edycji
+		$session = new Mmi_Session_Namespace('cms-language');
+		$lang = in_array($session->lang, Default_Registry::$config->application->languages) ? $session->lang : null;
+		if ($lang === null && isset(Default_Registry::$config->application->languages[0])) {
+			$lang = Default_Registry::$config->application->languages[0];
+		}
+		unset($request->lang);
+		unset(Mmi_Controller_Front::getInstance()->getRequest()->lang);
+		if ($lang !== null) {
+			Mmi_Controller_Front::getInstance()->getRequest()->lang = $lang;
+			$request->lang = $lang;
+		}
+
+		$this->view = Mmi_Controller_Front::getInstance()->getView();
+		$this->view->baseSkin = Default_Registry::$config->application->skin;
+		$this->view->baseModule = 'admin';
+
+		parent::__construct($request);
+	}
+
+}
