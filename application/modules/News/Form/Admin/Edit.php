f51a4e6834b5a8cb509d806335c95ba97a59633d
Bartlomiej Wolos <bwolos@empik.com>
2014-05-13 13:58:32 +0200

    Fix warto?ci w radio button

---
+++ application/modules/News/Form/Admin/Edit.php
@@ -0,0 +1,49 @@
+<?php
+
+class News_Form_Admin_Edit extends Mmi_Form {
+
+	protected $_recordName = 'News_Model_Record';
+
+	public function init() {
+
+		//ustawia zabezpieczenie CSRF
+		$this->setSecured();
+
+		$this->addElementText('title')
+			->setLabel('Tytuł artykułu')
+			->setRequired()
+			->addFilter('stringTrim')
+			->addValidatorNotEmpty();
+
+		$this->addElementCheckbox('internal')
+			->setLabel('Artykuł wewnętrzny')
+			->setValue(1);
+
+		$this->addElementText('uri')
+			->setLabel('Link do treści zewnętrznej');
+
+		$this->addElementTinyMce('lead')
+			->setLabel('Podsumowanie (zajawka)');
+
+		$this->addElementTinyMce('text')
+			->setLabel('Treść')
+			->setOption('img', 'news:' . $this->getRecord()->id)
+			->setRequired()
+			->setModeAdvanced();
+
+		$this->addElementSelect('visible')
+			->setLabel('Publikacja')
+			->setMultiOptions(array(
+				1 => 'włączony',
+				0 => 'wyłączony',
+			));
+
+		$this->addElementUploader('uploader')
+			->setLabel('Dołącz pliki');
+
+		$this->addElementSubmit('submit')
+			->setIgnore()
+			->setLabel('Zapisz');
+	}
+
+}
---
+++ application/modules/Tutorial/Form/Test.php
@@ -0,0 +1,23 @@
+<?php
+
+class Tutorial_Form_Test extends Mmi_Form {
+
+	protected $_recordName = 'Tutorial_Model_SimpleForm_Record';
+
+//	protected $_recordSaveMethod = 'saveWithPerson';
+
+	public function init() {
+
+		$this->addElementText('data')
+			->setLabel('Wpisz jakieś dane:')
+			->addFilter('stringTrim')
+			->setDescription('')
+			->setValue('')
+			->setRequired()
+			->addValidatorStringLength(2, 128, 'Wprowadź poprawne dane');
+
+		$this->addElementSubmit('add')
+			->setLabel('dodaj');
+	}
+
+}
