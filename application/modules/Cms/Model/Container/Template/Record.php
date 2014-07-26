1d094eb45828212200ac000d88a4600c309f0cc6
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2014-07-26 10:49:48 +0200

    drobne fixy w kontenerach stron

--- application/modules/Cms/Model/Container/Template/Record.php
+++ application/modules/Cms/Model/Container/Template/Record.php
@@ -12,6 +12,9 @@ class Cms_Model_Container_Template_Record extends Mmi_Dao_Record {
 		$uf = new Mmi_Filter_Url();
 		$this->path = DATA_PATH . '/cms-page-template-' . $uf->filter($this->name) . '.tpl';
 		file_put_contents($this->path, $this->text);
+		foreach (glob(TMP_PATH . '/compile/*.php') as $compilant) {
+			unlink($compilant);
+		}
 		return parent::save();
 	}
 	
