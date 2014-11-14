2f13e85d98cf708a3307f4b4a9eb12616db77226
mariusz <mariusz@milejko.pl>
2014-11-14 11:27:42 +0100

    drobne fixy unset

--- application/modules/Cms/Model/Acl/Record.php
+++ application/modules/Cms/Model/Acl/Record.php
@@ -46,7 +46,6 @@ class Cms_Model_Acl_Record extends Mmi_Dao_Record {
 			$this->module = isset($object[0]) ? strtolower($object[0]) : null;
 			$this->controller = isset($object[1]) ? strtolower($object[1]) : null;
 			$this->action = isset($object[2]) ? strtolower($object[2]) : null;
-			unset($this->object);
 		}
 		$this->_clearCache();
 		return parent::save();
