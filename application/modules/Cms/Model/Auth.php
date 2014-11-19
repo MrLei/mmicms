2c1f3ef1580d533eb04523e2c3193e577ec2c0f8
Mariusz Mi?ejko <mariusz@milejko.pl>
2014-11-19 14:31:26 +0100

    fix grida

--- application/modules/Cms/Model/Auth.php
+++ application/modules/Cms/Model/Auth.php
@@ -38,7 +38,7 @@ class Cms_Model_Auth implements Mmi_Auth_Model_Interface {
 				'message' => 'LOGIN FAILED: ' . $identity));
 			return false;
 		}
-		$record->roles = Cms_Model_Auth_Role_Dao::findPairsRolesByAuthId($record->id);
+		$record->setOption('roles', Cms_Model_Auth_Role_Dao::findPairsRolesByAuthId($record->id));
 		$record->lastIp = Mmi_Controller_Front::getInstance()->getEnvironment()->remoteAddress;
 		$record->lastLog = date('Y-m-d H:i:s');
 		Cms_Model_Log_Dao::add('login', array(
@@ -48,7 +48,11 @@ class Cms_Model_Auth implements Mmi_Auth_Model_Interface {
 			'success' => true,
 			'message' => 'LOGGED: ' . $record->username
 		));
-		return $record;
+		$authObject = new stdClass();
+		foreach ($record->toArray() as $key => $value) {
+			$authObject->$key = $value;
+		}
+		return $authObject;
 	}
 
 	public static function idAuthenticate($id) {
@@ -60,7 +64,7 @@ class Cms_Model_Auth implements Mmi_Auth_Model_Interface {
 		if ($record === null) {
 			return false;
 		}
-		$record->roles = Cms_Model_Auth_Role_Dao::findPairsRolesByAuthId($record->id);
+		$record->setOption('roles', Cms_Model_Auth_Role_Dao::findPairsRolesByAuthId($record->id));
 		$record->lastIp = Mmi_Controller_Front::getInstance()->getEnvironment()->remoteAddress;
 		$record->lastLog = date('Y-m-d H:i:s');
 		Cms_Model_Log_Dao::add('login', array(
