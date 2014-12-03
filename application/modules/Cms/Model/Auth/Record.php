03c973a3e12c5cbc62b33bd54c428b575d2d4c69
Mariusz Mi?ejko <mariusz@milejko.pl>
2014-12-03 11:54:47 +0100

    zmiana has?a u?ytkownika

--- application/modules/Cms/Model/Auth/Record.php
+++ application/modules/Cms/Model/Auth/Record.php
@@ -41,7 +41,7 @@ class Cms_Model_Auth_Record extends Mmi_Dao_Record {
 
 	public function changePasswordByUser() {
 		$auth = new Cms_Model_Auth();
-		$record = $auth->authenticate($this->identity, $this->password);
+		$record = $auth->authenticate($this->getOption('identity'), $this->password);
 		if ($record === false) {
 			$this->_setSaveStatus(-1);
 			return false;
@@ -50,9 +50,9 @@ class Cms_Model_Auth_Record extends Mmi_Dao_Record {
 			$this->_setSaveStatus(-2);
 			return false;
 		}
-		$auth = new self($record->id);
-		$auth->password = Cms_Model_Auth::getSaltedPasswordHash($this->getOption('changePassword'));
-		return $auth->save();
+		$authRecord = new self($record->id);
+		$authRecord->password = Cms_Model_Auth::getSaltedPasswordHash($this->getOption('changePassword'));
+		return $authRecord->save();
 	}
 
 	public function login() {
