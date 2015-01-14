a596eac5fb89d3b265110b8bb19cd66cc56946a5
Mariusz Mi?ejko <mariusz@milejko.pl>
2015-01-14 15:13:05 +0100

    Kontroler fronowy i backendowy cms_page

---
+++ application/modules/Cms/Controller/AdminPage.php
@@ -0,0 +1,17 @@
+<?php
+
+class Cms_Controller_AdminPage extends MmiCms_Controller_Admin {
+
+	public function indexAction() {
+		
+	}
+	
+	public function editAction() {
+		
+	}
+	
+	public function deleteAction() {
+		
+	}
+	
+}
