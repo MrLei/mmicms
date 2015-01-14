e4442ebfcc7bfa5eaefd6d69eed09925041bd84a
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-14 14:17:08 +0100

    Zbuildowane CMS_PAGE

---
+++ application/modules/Cms/Model/Page/Widget/Record.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cms_Model_Page_Widget_Record extends Mmi_Dao_Record {
+
+	public $id;
+	public $name;
+	public $module;
+	public $controller;
+	public $action;
+	public $params;
+	public $active;
+	public $cmsAuthId;
+
+}
