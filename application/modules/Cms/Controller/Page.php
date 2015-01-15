9b0076f9806ad3c9eb59d663be7228373c0dff1c
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-15 16:13:10 +0100

    Merge

--- application/modules/Cms/Controller/Page.php
+++ application/modules/Cms/Controller/Page.php
@@ -3,7 +3,7 @@
 class Cms_Controller_Page extends Mmi_Controller_Action {
 
 	public function indexAction() {
-		if (!$this->id || null === ($page = Cms_Model_Page_Dao::findPk($this->id))) {
+		if (!$this->id || null === ($page = Cms_Model_Page_Dao::activeByIdQuery($this->id)->findFirst())) {
 			$this->_helper->redirector('index', 'error', 'default', array(), true);
 		}
 		/* @var $page Cms_Model_Page_Record */
