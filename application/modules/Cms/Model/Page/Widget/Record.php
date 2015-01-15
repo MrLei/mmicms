8de03e1d15448907ca4011eec36566c2dda6b092
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-15 16:12:21 +0100

    Admin widgetow widocznych dla userow

--- application/modules/Cms/Model/Page/Widget/Record.php
+++ application/modules/Cms/Model/Page/Widget/Record.php
@@ -11,4 +11,16 @@ class Cms_Model_Page_Widget_Record extends Mmi_Dao_Record {
 	public $active;
 	public $cmsAuthId;
 
+	public function save() {
+		if ($this->getOption('widget')) {
+			$widget = explode(':', $this->getOption('widget'));
+			$this->module = strtolower($widget[0]);
+			$this->controller = strtolower($widget[1]);
+			$this->action = $widget[2];
+		}
+		$this->cmsAuthId = Default_Registry::$auth->getId();
+			
+		return parent::save();
+	}	
+	
 }
