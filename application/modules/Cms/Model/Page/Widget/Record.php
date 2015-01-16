035509cbcbad6d597e91825b8dc318a4d0ab5095
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-16 13:43:28 +0100

    poprawka rekordu cms_page po zmianie tabelek

--- application/modules/Cms/Model/Page/Widget/Record.php
+++ application/modules/Cms/Model/Page/Widget/Record.php
@@ -17,7 +17,6 @@ class Cms_Model_Page_Widget_Record extends Mmi_Dao_Record {
 			$this->controller = strtolower($widget[1]);
 			$this->action = $widget[2];
 		}
-		$this->cmsAuthId = Default_Registry::$auth->getId();
 			
 		return parent::save();
 	}	
