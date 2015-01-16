b0bf3e12e5a0fb36bc08390f928c4c670a36f454
mariusz <mariusz@milejko.pl>
2015-01-16 15:23:32 +0100

    cms page

--- application/modules/Cms/Model/Page/Dao.php
+++ application/modules/Cms/Model/Page/Dao.php
@@ -4,6 +4,17 @@ class Cms_Model_Page_Dao extends Mmi_Dao {
 
 	protected static $_tableName = 'cms_page';
 	
+	public static function findFirstById($id) {
+		$cacheKey = 'Cms_Page_' . $id;
+		if (null !== ($record = Default_Registry::$cache->load($cacheKey))) {
+			return $record;
+		}
+		$record = self::activeByIdQuery($id)
+			->findFirst();
+		Default_Registry::$cache->save($record, $cacheKey, 14400);
+		return $record;
+	}
+	
 	/**
 	 * 
 	 * @return Cms_Model_Page_Query
