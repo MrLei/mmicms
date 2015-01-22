297ad96de5354d89e189fd8bf7a4d3024ffc4894
Mariusz Mi?ejko <mariusz@milejko.pl>
2015-01-22 14:58:25 +0100

    dzia?aj?cy system przestawie? placeholder?w i sekcji

--- public/default/cms/js/page.js
+++ public/default/cms/js/page.js
@@ -1,81 +1,159 @@
+
+var CMSADMIN = CMSADMIN || {};
+
+CMSADMIN.composer = function () {
+	"use strict";
+	
+	var that = {},
+			init,
+			bind,
+			unbind,
+			toggle,
+			takenSpaceInSection,
+			root;
+	
+	init = function () {
+		 root = $('.cms-page-composer');
+		 return that;
+	};
+	that.init = init;
+
+	bind = function () {
+
+		root.find('.section').sortable({
+			items: '> div.placeholder',
+			opacity: 0.5,
+			axis: 'x',
+			tolerance: 'pointer',
+			placeholder: 'holder',
+			start: function (event, ui) {
+				$('.holder').addClass(ui.item[0].className);
+			}
+		});
+
+		root.sortable({
+			items: '> section',
+			opacity: 0.5,
+			axis: 'y',
+			tolerance: 'pointer',
+			placeholder: 'holder section'
+		});
+
+		root.find('div.placeholder').sortable({
+			items: '> section',
+			opacity: 0.5,
+			axis: 'y',
+			tolerance: 'pointer',
+			placeholder: 'holder section'
+		});
+
+		root.find('div.placeholder').resizable({
+			handles: 'e',
+			create: function (event, ui) {
+				$(this).resizable('option', 'grid', $(this).parent().width() / 12);
+			},
+			resize: function (event, ui) {
+				//obliczanie bieżącej szerokości
+				var currentWidth = Math.round($(this).width() / ($(this).parent().width() / 12));
+
+				//czyszczenie css po obliczeniach na podstawie rozmiaru
+				$(this).parent().find('div.placeholder').removeAttr('data-current');
+				$(this).parent().find('div.placeholder').removeAttr('style');
+				$(this).attr('data-pool-excluded', '1');
+
+				//przestawienie szerokości jeśli > 1 i nie przepełnia puli
+				if (currentWidth > 0 && (currentWidth + takenSpaceInSection($(this).parent())) < 13) {
+					$(this).removeAttr('class');
+					$(this).addClass('placeholder ui-resizable span-' + currentWidth + '-of-12');
+				}
+				$(this).removeAttr('data-pool-excluded');
+			}
+		});
+
+		root.find('> .section > div.placeholder').droppable({
+			accept: '.template-section',
+			greedy: true,
+			tolerance: 'pointer',
+			drop: function (event, ui) {
+				$(this).prepend('<section class="section"></section>');
+				unbind();
+				bind();
+			}
+		});
+
+		root.find('.section').droppable({
+			accept: '.template-placeholder',
+			greedy: true,
+			tolerance: 'pointer',
+			drop: function (event, ui) {
+				var freeSpace = 12 - takenSpaceInSection($(this));
+				if (freeSpace <= 0) {
+					return false;
+				}
+				console.log(freeSpace);
+				$(this).append('<div class="placeholder span-' + freeSpace + '-of-12"></div>');
+				unbind();
+				bind();
+			}
+		});
+
+		root.find('.section, div.placeholder').on('dblclick', function () {
+			$(this).remove();
+			return false;
+		});
+
+		root.addClass('compose');
+
+	};
+	that.bind = bind;
+
+	unbind = function () {
+		if (!root.hasClass('compose')) {
+			return;
+		}
+		root.removeClass('compose');
+		root.find('.section, div.placeholder').off('dblclick');
+		root.sortable().sortable('destroy');
+		root.find('div.placeholder, .section').sortable().sortable('destroy');
+		root.find('div.placeholder').resizable().resizable('destroy');
+		root.find('div.placeholder, .section').droppable().droppable('destroy');
+	};
+	that.unbind = unbind;
+	
+	takenSpaceInSection = function (element) {
+		var pool = 0;
+		element.find('> div.placeholder').each(function () {
+			if (!($(this).attr('data-pool-excluded') === '1')) {
+				pool = pool + parseInt($(this).attr('class').match(/span\-([1-9]|10|11|12)\-of\-12/)[1]);
+			}
+		});
+		return pool;
+	}
+	
+	toggle = function () {
+		if (!root.hasClass('compose')) {
+			return bind();
+		}
+		return unbind();
+	};
+	that.toggle = toggle;
+
+	return that;
+};
+
 $(document).ready(function () {
 
-    $('.section').sortable({
-        items: '> div.placeholder',
-        opacity: 0.5,
-        axis: 'x',
-        placeholder: 'holder',
-        start: function (event, ui) {
-            $('.holder').addClass(ui.item[0].className);
-        }
-    });
+	CMSADMIN.composer = CMSADMIN.composer().init();
+	
+	CMSADMIN.composer.bind();
 
-    $('div').sortable({
-        items: '> section',
-        opacity: 0.5,
-        axis: 'y',
-        placeholder: 'holder section',
-    });
-
-    $('div.placeholder').resizable({
-		handles: 'e',
-		create: function (event, ui) {
-			$(this).resizable('option', 'grid', $(this).parent().width() / 12);
-		},
-		resize: function (event, ui) {
-			//obliczanie bieżącej szerokości
-			var currentWidth = Math.round($(this).width() / ($(this).parent().width() / 12));
-			var pool = currentWidth;
-			
-			//czyszczenie css po obliczeniach na podstawie rozmiaru
-			$(this).parent().find('div.placeholder').removeAttr('data-current');
-			$(this).parent().find('div.placeholder').removeAttr('style');
-
-			$(this).attr('data-current', 'current')
-
-			//obliczanie puli wolnego miejsca w sekcji
-			$(this).parent().find('> div.placeholder').each(function () {
-				if (!$(this).attr('data-current')) {
-					pool = pool + parseInt($(this).attr('class').match(/span\-([1-9]|10|11|12)\-of\-12/)[1]);
-				}
-			});
-
-			//przestawienie szerokości jeśli > 1 i nie przepełnia puli
-			if (currentWidth > 0 && pool < 13) {
-				$(this).removeAttr('class');
-				$(this).addClass('placeholder ui-resizable span-' + currentWidth + '-of-12');
-			}
-		}
-    });
-
-	$('div.placeholder').droppable({
-		accept: '.template-section',
-		drop: function (event, ui) {
-			alert('dropped section on placeholder');
-		}
+	$('.fix-button').click(function () {
+		CMSADMIN.composer.toggle();
 	});
-
-	$('.section').droppable({
-		accept: '.template-placeholder',
-		drop: function (event, ui) {
-			alert('dropped placeholder on section');
-		}
-	});
-
-	$('.section, div.placeholder').on('dblclick', function () {
-        $(this).remove();
-        return false;
-    });
-
-    $('#cms-page-composer').addClass('compose');
-
+	
 	$('.drag').draggable({
 		revert: true,
 		snap: true
 	});
-
-    $('.fix-button').click(function () {
-        $('#cms-page-composer').toggleClass('compose');
-    });
 
 });
