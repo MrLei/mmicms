879659f4ee57d7909735b675d296dbd1f7c8282b
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-23 14:07:56 +0100

    podstawowe widgety - poczatek

---
+++ application/modules/Cms/Controller/AdminWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+class Cms_Controller_AdminWidget extends MmiCms_Controller_Admin {
+	
+	public function indexAction() {
+	}
+	
+	public function textWidgetEditAction() {
+		$form = new Cms_Form_Admin_Widget_Text();
+	}
+	
+	public function pictureWidgetEditAction() {
+		
+	}
+	
+}
