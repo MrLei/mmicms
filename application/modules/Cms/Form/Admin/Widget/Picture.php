8302a25423669913f65857ff9b4ef54a8ad5fcea
Maciej Paw?owski <maciej.pawlowski2@empik.com>
2015-01-28 16:09:05 +0100

    cd. widget picture

--- application/modules/Cms/Form/Admin/Widget/Picture.php
+++ application/modules/Cms/Form/Admin/Widget/Picture.php
@@ -1,16 +1,27 @@
 <?php
 
-class Cms_Form_Admin_Widget_Picture extends MmiCms_Form {
+class Cms_Form_Admin_Widget_Picture extends Mmi_Form {
 
 	protected $_recordName = 'Cms_Model_Widget_Picture_Record';
-	
+
 	public function init() {
 
-		$this->addElementUploader('uploader')
-			->setLabel('Załaduj');
+		$this->addElementFile('cmswidgetpicture')
+			->setLabel('Dodaj zdjęcie')
+			->setRequired();
 
 		$this->addElementSubmit('submit')
 			->setLabel('Zapisz zdjęcie');
+	}
+
+	protected function _appendFiles($id, $files) {
+		if (empty($files)) {
+			return;
+		}
+		$object = 'cmswidgetpicture';
+		//zastapienie obecnego pliku
+//		Cms_Model_File_Dao::imagesByObjectQuery($object, $id)->find();
+//		Cms_Model_File_Dao::appendFiles($object, $id, $files);
 	}
 
 }
---
+++ application/modules/Cms/Plugin/PictureWidgetGrid.php
@@ -0,0 +1,32 @@
+<?php
+
+class Cms_Plugin_PictureWidgetGrid extends Mmi_Grid {
+	
+	public function init() {
+		
+		$this->setQuery(Cms_Model_Widget_Picture_Query::factory()
+			->join('cms_file')->on('id', 'objectId')
+			->where('object', 'cms_file')->equals('cmswidgetpicture'));
+		
+		$this->addColumn('text', 'id', [
+			'label' => 'ID zdjecia'
+		]);
+		
+		$this->addColumn('custom', 'picture', [
+			'label' => 'Zdjecie',
+			'seekable' => 'false',
+			'sortable' => 'false',
+			'value' => '<img src="{$rowData->getJoined(\'cms_file\')->getUrl(\'scale\',\'150\')}">'
+		]);
+		
+		$this->addColumn('buttons', 'buttons', [
+			'label' => 'Operacje',
+			'links' => [
+				'edit' => '',
+				'delete' => $this->_view->url(array('id' => '%id%', 'action' => 'pictureWidgetDelete', 'controller' => 'adminWidget', 'module' => 'cms'))
+			]
+		]);
+		
+	}
+	
+}
