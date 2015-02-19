19549860650d95e8d040d5ae56fcec184cb01f0d
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2015-02-19 14:40:47 +0100

    ulepszona dokumentacja

--- application/modules/Default/Config/Navigation.php
+++ application/modules/Default/Config/Navigation.php
@@ -1,8 +1,15 @@
 <?php
 
+/**
+ * Klasa konfiguracji nawigatora
+ */
 class Default_Config_Navigation extends Mmi_Navigation_Config {
 
+	/**
+	 * Konstruktor inicjujący konfigurację
+	 */
 	public function __construct() {
+		//dodanie menu skonfigurowanego w module CMS
 		$this->addElement(Cms_Config_Admin_Navigation::getMenu());
 	}
 
