1ab93143bc7c373fad809f86a9672c145ba2cf80
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2015-02-19 14:40:47 +0100

    ulepszona dokumentacja

--- application/modules/Default/Config/Router.php
+++ application/modules/Default/Config/Router.php
@@ -1,13 +1,23 @@
 <?php
 
+/**
+ * Klasa konfiguracji routera
+ */
 class Default_Config_Router extends Mmi_Controller_Router_Config {
 
+	/**
+	 * Konstruktor konfigurujący router
+	 */
 	public function __construct() {
 
+		//strona główna
 		$this->setRoute(0, '', array('module' => 'default'), array('controller' => 'index', 'action' => 'index'));
+		
+		//artykuły CMS
 		$this->setRoute(1, '/^strona,(.[^\/]+)$/', array('module' => 'cms', 'controller' => 'article', 'action' => 'index', 'uri' => '$1'));
 		$this->setRoute(2, '/^strona,(.[^\/]+)$/', array('module' => 'cms', 'controller' => 'article', 'action' => 'index', 'uri' => '$1'));
 
+		//aktualności CMS
 		$this->setRoute(10, 'aktualnosci', array('module' => 'cms', 'controller' => 'news', 'action' => 'index'));
 		$this->setRoute(11, '/^aktualnosci-([0-9]+)\/([0-9]+)/', array('module' => 'cms', 'controller' => 'news', 'action' => 'index', 'pages' => '$1', 'p' => '$2'));
 		$this->setRoute(12, '/^aktualnosci-([0-9]+)/', array('module' => 'cms', 'controller' => 'news', 'action' => 'index', 'pages' => '$1'));
