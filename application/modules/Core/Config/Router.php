1859453b85b66faa042cd6a68868b06ed96629e7
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2015-03-03 16:39:20 +0100

    uzupe?nione komentarze

--- application/modules/Core/Config/Router.php
+++ application/modules/Core/Config/Router.php
@@ -1,7 +1,11 @@
 <?php
 
 /**
- * Klasa konfiguracji routera
+ * Mmi Framework (https://code.google.com/p/mmicms/)
+ * 
+ * @link       https://code.google.com/p/mmicms/
+ * @copyright  Copyright (c) 2010-2014 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
  */
 
 namespace Core\Config;
