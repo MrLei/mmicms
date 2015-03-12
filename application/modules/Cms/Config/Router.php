81781c47d177f0987adfe21da8d76639df164438
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2015-03-12 13:32:15 +0100

    komentarze w routerze

--- application/modules/Cms/Config/Router.php
+++ application/modules/Cms/Config/Router.php
@@ -10,16 +10,16 @@ class Router extends \Mmi\Controller\Router\Config {
 
 	public function __construct() {
 		
-		//strona główna: /
+		//strona główna moduł core + kontroler index + akcja index
 		$this->setRoute(110000, '/^$/', array('module' => 'core'), array('controller' => 'index', 'action' => 'index'));
 		
-		//moduł (index/index): /news
+		//moduł + kontroler index + akcja index np. /cms
 		$this->setRoute(110001, '/^([a-zA-Z]+)$/', array('module' => '$1'), array('controller' => 'index', 'action' => 'index'));
 		
-		//moduł + kontroler + akcja index: /news/article
+		//moduł + kontroler + akcja index np. /cms/article
 		$this->setRoute(110002, '/^([a-zA-Z]+)\/([a-zA-Z\-]+)$/', array('module' => '$1', 'controller' => '$2'), array('action' => 'index'));
 		
-		//moduł + kontroler + akcja: /news/article/display
+		//moduł + kontroler + akcja np. /cms/article/display
 		$this->setRoute(110003, '/^([a-zA-Z]+)\/([a-zA-Z\-]+)\/([a-zA-Z]+)$/', array('module' => '$1', 'controller' => '$2', 'action' => '$3'));
 		
 	}
