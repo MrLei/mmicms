fd5e92566d0c90149cfcb8873ab5be303ae8515f
Mariusz Mi?ejko <Mariusz Mi?ejko@localhost>
2015-03-13 18:01:53 +0100

    refaktoryzacja rekordu strony cms

--- application/modules/Cms/Model/Page/Record.php
+++ application/modules/Cms/Model/Page/Record.php
@@ -23,10 +23,14 @@ class Record extends \Mmi\Dao\Record {
 	public $cmsAuthId;
 
 	public function saveForm() {
+		
+		//pobieranie elementu nawigacji
 		$navigationRecord = $this->cmsNavigationId ? \Cms\Model\Navigation\Query::factory()->findPk($this->cmsNavigationId) : null;
-		/* @var $navigationRecord \Cms\Model\Navigation\Record */
-		$navigationRecord = ($navigationRecord === null) ? new \Cms\Model\Navigation\Record() : $navigationRecord;
-
+		//jeśli nie pobrano - nowy
+		if ($navigationRecord === null) {
+			$navigationRecord = new \Cms\Model\Navigation\Record();
+		}
+		//ustawianie opcji elementu
 		$navigationRecord->absolute = 0;
 		$navigationRecord->action = 'index';
 		$navigationRecord->active = 1;
@@ -42,48 +46,70 @@ class Record extends \Mmi\Dao\Record {
 		$navigationRecord->visible = 0;
 		$navigationRecord->save();
 
+		//pobieranie routy
 		$routeRecord = $this->cmsRouteId ? \Cms\Model\Route\Query::factory()->findPk($this->cmsRouteId) : null;
-		/* @var $routeRecord \Cms\Model\Route\Record */
-		$routeRecord = ($routeRecord === null) ? new \Cms\Model\Route\Record() : $routeRecord;
+		//jeśli nie pobrano
+		if ($routeRecord === null) {
+			$routeRecord = new \Cms\Model\Route\Record();
+		}
+		//ustawianie routy
 		$routeRecord->active = $this->active;
 		$routeRecord->pattern = $this->getOption('address');
 		$routeRecord->save();
 
+		//przypisanie routy i elementu nawigacji
 		$this->cmsNavigationId = $navigationRecord->id;
 		$this->cmsRouteId = $routeRecord->id;
+		//ustawienie właściciela
 		$this->cmsAuthId = \Core\Registry::$auth->getId();
 		$this->save();
+		
+		//zapis do rekordu nawigacji parametru ID strony
 		$navigationRecord->params = 'id=' . $this->id;
 		if (!$navigationRecord->order) {
 			$navigationRecord->order = 10000;
 		}
+		
+		//zapis do rekordu routy ID
 		$routeRecord->replace = 'module=cms&controller=page&action=index&id=' . $this->id;
 		if (!$routeRecord->order) {
 			$routeRecord->order = 10000;
 		}
+		//zapis elementu nawigacyjnego i routy
 		return $navigationRecord->save() && $routeRecord->save();
 	}
 
+	/**
+	 * Usuwanie rekordu
+	 * @return boolean
+	 */
 	public function delete() {
+		//uwsuwanie standardowego rekordu
 		if (!parent::delete()) {
 			return false;
 		}
+		//usuwa powiązany element nawigacyjny
 		$navigationRecord = \Cms\Model\Navigation\Query::factory()->findPk($this->cmsNavigationId);
-		if ($navigationRecord !== null) {
-			$navigationRecord->delete();
-		}
+		$navigationRecord !== null && $navigationRecord->delete();
+		//usuwa powiązaną routę
 		$routeRecord = \Cms\Model\Route\Query::factory()->findPk($this->cmsRouteId);
-		if ($routeRecord !== null) {
-			$routeRecord->delete();
-		}
+		$routeRecord !== null && $routeRecord->delete();
 		return true;
 	}
 
+	/**
+	 * Ustawia datę dodania
+	 * @return boolean
+	 */
 	protected function _insert() {
 		$this->dateAdd = date('Y-m-d H:i:s');
 		return parent::_insert();
 	}
 
+	/**
+	 * Ustawia datę modyfikacji
+	 * @return boolean
+	 */
 	protected function _update() {
 		$this->dateModify = date('Y-m-d H:i:s');
 		return parent::_update();
